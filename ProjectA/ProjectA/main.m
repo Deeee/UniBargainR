c176af79c88d5c24be24ea5dca9d1e888189bb0f
a505190722 <liudii@Me.com>
2014-06-20 21:45:18 -0400

    First Commit

---
+++ ProjectA/ProjectA/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ProjectA
+//
+//  Created by Liu Di on 6/20/14.
+//  Copyright (c) 2014 Liu Di. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
