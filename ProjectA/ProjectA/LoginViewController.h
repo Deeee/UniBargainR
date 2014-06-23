383b17d9a5be3c0097ab944912a1959e8815b329
a505190722 <liudii@Me.com>
2014-06-22 21:53:37 -0400

    added register and login

---
+++ ProjectA/ProjectA/LoginViewController.h
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.h
+//  ProjectA
+//
+//  Created by Liu Di on 6/21/14.
+//  Copyright (c) 2014 Liu Di. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+@property(nonatomic, strong) IBOutlet UITextField *userName;
+@property(nonatomic, strong) IBOutlet UITextField *userPassword;
+@end
---
+++ ProjectA/ProjectA/LoginViewController.m
@@ -0,0 +1,53 @@
+//
+//  LoginViewController.m
+//  ProjectA
+//
+//  Created by Liu Di on 6/21/14.
+//  Copyright (c) 2014 Liu Di. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+@synthesize userName;
+@synthesize userPassword;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.userPassword.placeholder = [NSString stringWithFormat:@"Enter Password"];
+    self.userName.placeholder = [NSString stringWithFormat:@"Enter Username"];
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
